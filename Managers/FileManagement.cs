10fce891fc3d9c209dce6845c830f5b5dbb67734
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-02 17:20:50 -0300

    Add FileManagement

--- Managers/FileManagement.cs
+++ Managers/FileManagement.cs
@@ -1,7 +1,26 @@
-﻿namespace BaseConverter.Managers
+﻿using BaseConverter.Global;
+
+namespace BaseConverter.Managers
 {
     public static class FileManagement
     {
-        
+        public static string[] GetLines(string path)
+        {
+            try
+            {
+                return File.ReadAllLines(GlobalVariables.PathConversao);
+            }
+            catch (Exception ex) when (ex is IOException)
+            {
+                Console.WriteLine("Provavelmente o arquivo está sendo usado em outro programa. Por favor, feche-o.");
+                Thread.Sleep(2000);
+                return GetLines(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("O seguinte erro ocorreu: " + ex.Message);
+                return [];
+            }
+        }
     }
 }
