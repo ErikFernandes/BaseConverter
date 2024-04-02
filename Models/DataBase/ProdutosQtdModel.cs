f80cec7ececd3863b8f868f47bf77ebec3f48c21
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-02 17:40:49 -0300

    Refactoring Models

--- Models/DataBase/ProdutosQtdModel.cs
+++ Models/DataBase/ProdutosQtdModel.cs
@@ -8,7 +8,7 @@
         public decimal Quantidade { get; set; } = 0.00m;
         public decimal EstoqueMaximo { get; set; } = 0.00m;
         public decimal EstoqueMinimo { get; set; } = 0.00m;
-        public string Prateleira { get; set; } = "";
+        public string Prateleira { get; set; } = string.Empty;
         public bool Ativo { get; set; } = true;
     }
 }
