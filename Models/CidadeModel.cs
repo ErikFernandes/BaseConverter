a0d88d33695c95abfe50723401c107b1954161aa
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-02 17:41:04 -0300

    Add IBGE Info

---
+++ Models/CidadeModel.cs
@@ -0,0 +1,11 @@
+﻿using BaseConverter.Enums;
+
+namespace BaseConverter.Models
+{
+    public class CidadeModel
+    {
+        public EstadosEnum UF { get; set; }
+        public string CodMunicipio { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+    }
+}
