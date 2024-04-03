1e636cf634f27f8b6c2c35052bcdeca17a927c4b
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-03 12:30:45 -0300

    Create ListExtension.cs

---
+++ Extensions/ListExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace BaseConverter.Extensions
+{
+    public static class ListExtension
+    {
+        /// <summary>
+        /// Adits the <paramref name="value"/> to the list if it is not already in it.
+        /// </summary>
+        /// <param name="list">List to be modified.</param>
+        /// <param name="value">Value to be added.</param>
+        public static void AddIfNotExists<T>(this List<T> list, T value)
+        { if (!list.Contains(value)) { list.Add(value); } }
+        
+    }
+}
