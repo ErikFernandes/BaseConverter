65e2d8928b7d453b61fea05f81ba1d99f446021c
Érik Fernandes <erikhenriquemourafernandes@gmail.com>
2024-04-05 10:46:34 -0300

    Add CodigosModel

---
+++ Models/DataBase/CodigosModel.cs
@@ -0,0 +1,15 @@
+﻿using BaseConverter.Global;
+
+namespace BaseConverter.Models
+{
+    public class CodigosModel
+    {
+        public int IdProd { get; set; } = GlobalVariables.CurrentIdProdutos;
+        public int IdProdQtd { get; set; } = GlobalVariables.CurrentIdProdutosQtd;
+        public int IdDepartamento { get; set; } = GlobalVariables.CurrentIdDepartamentos;
+        public int IdCategoria { get; set; } = GlobalVariables.CurrentIdCategorias;
+        public int IdMarca { get; set; } = GlobalVariables.CurrentIdMarcas;
+        public int IdUnidade { get; set; } = GlobalVariables.CurrentIdUnidades;
+
+    }
+}
